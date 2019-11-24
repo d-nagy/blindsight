efe83337a025f44069281a8c7008dd56de47fd12
JamesMax314 <10james.maxwell@gmail.com>
2019-11-24 05:08:20 +0000

    speech to text

--- public/js/webSpeech.js
+++ public/js/webSpeech.js
@@ -1,5 +1,9 @@
 let synth = window.speechSynthesis;
 
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
 document.body.onclick = function() {
     let text = "I am not happy";
     let utterance = new SpeechSynthesisUtterance(text);
@@ -10,3 +14,30 @@ document.body.onclick = function() {
     utterance.rate = 0.6;
     synth.speak(utterance);
 };
+
+var startwrd = "blind";
+var grammar = '#JSGF V1.0; grammar startwrd; public <startwrd> = ' + startwrd + ' ;'
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+
+recognition.grammars = speechRecognitionList;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event) {
+  var last = event.results.length - 1;
+  var word = event.results[last][0].transcript;
+  console.log(word);
+}
+
+recognition.onnomatch = function(event) {
+  console.log('I didnt recognise that color.');
+}
